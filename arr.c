b0cb8ddbbd1343331f7a56dc746587d95c768c00
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-06-06 22:34:59 +0530

    e

--- arr.c
+++ arr.c
@@ -2,7 +2,7 @@
 int main(){
     int arr[2][4]={{1,2,3,4},
                     {5,6,7,8}};
-    printf("Element of an array A are\n");
+    printf("Element of an array arr are\n");
     int r,c;
     for(r=0;r<2;r++){
         for(c=0;c<4;c++){
