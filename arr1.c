9426d47f1bd4bd68cbb30560ab60e3d9a86a59aa
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-06-10 10:32:40 +0530

    a

--- arr1.c
+++ arr1.c
@@ -8,7 +8,7 @@ int main(){
             scanf("%d",&arr[r][c]);
         }
     }
-    printf("\n Saved elements in array array\n");
+    printf("\n Saved elements in array array!\n");
     for(r=0;r<3;r++){
         for(c=0;c<3;c++){
             printf("%d",arr[r][c]);
