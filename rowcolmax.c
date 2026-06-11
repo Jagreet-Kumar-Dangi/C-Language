5dd89b4865a14f1465a2f311fc1d192247928736
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-06-11 19:36:53 +0530

    a

--- rowcolmax.c
+++ rowcolmax.c
@@ -10,7 +10,7 @@ int main(){
             scanf("%d",&arr[i][j]);
         }
     }
-    printf("Array values is: \n");
+    printf("Array values is:\n");
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             printf("%d",arr[i][j]);
