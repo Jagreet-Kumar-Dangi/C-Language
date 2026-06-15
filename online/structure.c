a63cde9417fd91517063970cd34ed397c635ba93
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-06-15 21:53:02 +0530

    w

--- online/structure.c
+++ online/structure.c
@@ -16,7 +16,7 @@ int main()
     strcpy(s1.branch,"CSE");
     printf("\nName= %s\n",s1.name);
     printf("\nRegistration no.= %d\n",s1.reg);
-    printf("\nRoll No.= %d\n",s1.rno);
-    printf("\nBranch= %s\n",s1.branch);
+    printf("\nRoll No.=%d\n",s1.rno);
+    printf("\nBranch=%s\n",s1.branch);
 
 }
