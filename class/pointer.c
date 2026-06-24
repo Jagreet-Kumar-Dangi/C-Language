91aeb867e35a7e744258c68c36c660eaaa0be93c
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-06-24 22:14:19 +0530

    y

--- class/pointer.c
+++ class/pointer.c
@@ -12,7 +12,7 @@ int  main(){
     printf("\nValue of var a=%.1f",*(float *)p);
     p=&c;
     printf("\nAddress of var a=%d ",p);
-    printf("\nValue of var a=%c",*(char *)p);
+    printf("\nValue of var  a=%c",*(char *)p);
     int *q=NULL;
     
 
