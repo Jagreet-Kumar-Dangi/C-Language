eb53a92592103b425fbc249a9919cddccea053c6
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-07-03 22:44:22 +0530

    l

--- diasum.c
+++ diasum.c
@@ -4,7 +4,7 @@ int main(){
     printf("Enter rows and columns -:\n");
     scanf("%d %d",&n,&m);
     int arr[n][m];
-    printf("Enter the values: ");
+    printf("Enter the values-: ");
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             scanf("%d",&arr[i][j]);
