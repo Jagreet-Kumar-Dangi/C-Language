400665b784b40b0f525041943f93ea443223606b
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-07-05 22:20:38 +0530

    v

--- arr1.c
+++ arr1.c
@@ -2,7 +2,7 @@
 int main(){
     int arr[3][3];
     int r,c;
-    printf("Enter element in array!\n");
+    printf("Enter element in array-:\n");
     for(r=0;r<3;r++){
         for(c=0;c<3;c++){
             scanf("%d",&arr[r][c]);
