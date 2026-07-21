6dd6594cf9c591632856e13ca80c321ea9694f15
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-07-21 21:56:48 +0530

    b

--- class/pointer5.c
+++ class/pointer5.c
@@ -8,7 +8,7 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("Enter element to replace-:");
+    printf("Enter element to replace-: ");
     scanf("%d",&e);
     printf("Enter location to replace-: ");
     scanf("%d",&x);
