2b6bf16555fee8064f28764fec9abfe1c328e2eb
Jagreet Dangi <jagreetdangi2007@gmail.com>
2026-07-27 21:45:42 +0530

    k

--- class/pointer5.c
+++ class/pointer5.c
@@ -17,7 +17,7 @@ int main(){
     }else{
         arr[x]=e;
     }
-    printf("Array after replacement:");
+    printf("Array after replacement: ");
     for(int i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
